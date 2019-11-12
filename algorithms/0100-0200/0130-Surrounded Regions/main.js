a666cbb326d006944a5849ac013cd56f10567c29
unknown <snsart@163.com>
2019-11-12 19:21:23 +0800

    add

--- algorithms/0100-0200/0130-Surrounded Regions/main.js
+++ algorithms/0100-0200/0130-Surrounded Regions/main.js
@@ -3,20 +3,22 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solve = function(board) {
+    if(board==null||board.length<=2) return;
+    
     for(let i=0;i<board.length;i++){
         if(board[i][0]=="O"){
             dfs(board,i,0);
         }
-        if(board[i][board.length-1]=="O"){
-            dfs(board,i,board.length-1);
+        if(board[i][board[0].length-1]=="O"){
+            dfs(board,i,board[0].length-1);
         }
     }
     for(let i=0;i<board[0].length;i++){
         if(board[0][i]=="O"){
             dfs(board,0,i);
-        }i
-        if(board[board[0].lengt-1][i]=="O"){
-            dfs(board,board[0].lengt-1,i);
+        }
+        if(board[board.length-1][i]=="O"){
+            dfs(board,board.length-1,i);
         }
     }
     for(let i=0;i<board.length;i++){
@@ -24,7 +26,7 @@ var solve = function(board) {
             if(board[i][j]=="O"){
                 board[i][j]="X";
             }
-            if(board[i][j]="-"){
+            if(board[i][j]=="-"){
                 board[i][j]="O";
             }
         }
@@ -36,6 +38,7 @@ function dfs(board,i,j){
     if(i<0||i>board.length-1||j<0||j>board[0].length-1) return;
     if(board[i][j]!="O") return;
     if(board[i][j]=="O") board[i][j]="-";
+    
     dfs(board,i-1,j);
     dfs(board,i+1,j);
     dfs(board,i,j-1);
